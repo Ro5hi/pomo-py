deb123205d159bb97ecfdefe9a9e30b3bd74761b
Ro5hi <slen.detablan@gmail.com>
2021-05-22 14:54:39 -0600

    added after-break timer

--- pomo.py
+++ pomo.py
@@ -23,3 +23,12 @@ import time
             time.sleep(1)
             t -= 1
         print("Take a break!")
+        t = 5*60
+        while t:
+            mins = t // 60
+            secs = t % 60
+            timer = '{:02d}:{:02d}'.format(mins, secs)
+            print(timer, end="\r")
+            time.sleep(1)
+            t -= 1 
+        print("Back to work!")
