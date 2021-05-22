807a5dd05f96e97d3f948c972f8bbe84893aa1fd
Ro5hi <slen.detablan@gmail.com>
2021-05-22 14:47:09 -0600

    added time.sleep, t-= 1 to stop timer

--- pomo.py
+++ pomo.py
@@ -6,4 +6,8 @@ while t:
     mins = t // 60
     secs = t % 60
     timer = '{:02d}:{0:2d}'.format(mins, secs)
-    print(timer, end="\r")
+
+    print(timer, end="\r") 
+    time.sleep(1)
+    t -= 1    
+    print('Rest time!')
