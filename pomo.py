888083cd3e25bd6bce78f62512f6f4a635dc1334
Ro5hi <slen.detablan@gmail.com>
2021-05-22 14:45:16 -0600

    imported time, defined t, while t

---
+++ pomo.py
@@ -0,0 +1,9 @@
+import time
+
+t = int(input("How much time do you want to set?"))
+
+while t:
+    mins = t // 60
+    secs = t % 60
+    timer = '{:02d}:{0:2d}'.format(mins, secs)
+    print(timer, end="\r")
